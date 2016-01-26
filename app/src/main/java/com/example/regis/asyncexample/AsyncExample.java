f572ebe00fda1dbafa43b6d2f77571a3b72d72e2
Rex7 <regischarles7@gmail.com>
2016-01-26 22:33:55 +0530

    downloading image and showing that image into imageview

--- app/src/main/java/com/example/regis/asyncexample/AsyncExample.java
+++ app/src/main/java/com/example/regis/asyncexample/AsyncExample.java
@@ -8,20 +8,40 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 
-public class AsyncExample extends AppCompatActivity {
-    String d="http://www.cdn.actiontrip.com/images/babes/midsize/botd_01202015.jpg";
+public class AsyncExample extends AppCompatActivity implements AdapterView.OnItemClickListener{
+
+    ListView lv;
+    ImageView imageView;
+    String [] nameofactress;
+    String [] urlofimage;
+    //String d="http://www.cdn.actiontrip.com/images/babes/midsize/botd_01202015.jpg";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.async_demo);
-        MyAsync as = new MyAsync();
+        lv=(ListView)findViewById(R.id.ListItem);
+        nameofactress=getResources().getStringArray(R.array.Nameofactress);
+        urlofimage =getResources().getStringArray(R.array.url);
+        imageView=(ImageView)findViewById(R.id.ImageView1);
+        ArrayAdapter<String> ad = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,nameofactress);
+        lv.setAdapter(ad);
+        lv.setOnItemClickListener(this);
+        /* MyAsync as = new MyAsync();
         as.execute(d);
+        */
 
     }
     public InputStream connection(String urls)
@@ -60,6 +80,19 @@ public class AsyncExample extends AppCompatActivity {
         drawable = new BitmapDrawable(getResources(),ds);
         return drawable;
     }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+      TextView lss =(TextView) view;
+        Toast.makeText(getApplicationContext(),lss.getText(),Toast.LENGTH_LONG).show();
+        String link=urlofimage[i];
+        MyAsync as = new MyAsync();
+        as.execute(link);
+
+
+
+    }
+
     public class MyAsync extends AsyncTask<String,Void,Drawable>
     {
 
@@ -74,8 +107,9 @@ public class AsyncExample extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(Drawable drawable) {
-            RelativeLayout ls = (RelativeLayout)findViewById(R.id.Ral);
-            ls.setBackgroundDrawable(drawable);
+            imageView.setImageDrawable(drawable);
+           // RelativeLayout ls = (RelativeLayout)findViewById(R.id.Ral);
+            //ls.setBackgroundDrawable(drawable);
         }
     }
 }
